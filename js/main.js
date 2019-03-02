9237533a59e87943eb4a3efd2c6311e06ea496d5
Marija Bazovic <mbazovic@live.com>
2019-03-02 09:48:49 +0100

    blur, center, overlay dodato

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 let a = id => document.getElementById(id);
-
+let s = state => {
+  a('modal').style.display = state;
+  a('overlay').style.display = state;
+  if (state==='block') {
+    a('content').classList.add('blurred');
+  } else {
+    a('content').classList.remove('blurred');
+  }
+}
 a('btn').addEventListener('click', function () {
-  a('modal').style.display = "block";
+  s('block');
 });
 
 a('close').addEventListener('click', function () {
-  a('modal').style.display = "none";
+  s('none');
 });
 
 
-window.addEventListener('click', function (event)  {
-  if (event.target == a('modal')) {
-    a('modal').style.display = "none";
-  }
+a('overlay').addEventListener('click', function (event)  {
+  s('none');
 })
 
 
